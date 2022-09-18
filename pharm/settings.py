5e0deeda680873b745ed4d4490fdd489f938fcb5
tiaraRS <tiara.rojas@ucb.edu.bo>
2022-09-18 07:17:43 -0400

    SECURITY: Make sure this debug feature is deactivated before delivering the code in production. solved

--- pharm/settings.py
+++ pharm/settings.py
@@ -24,6 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ui1u$88tg97g-()a(^hcv5mn#jzcu^3yb1n=!s7kn-!t25932n'
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
 # ALLOWED_HOSTS = ['pharmacymanage.herokuapp.com']
 ALLOWED_HOSTS = [
     'pharmacy-management-system2.herokuapp.com',
